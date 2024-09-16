a3727e38ae54595139b74d58db4f2572de915465
WilfredoAaronSosaRamosBoushtech <wilfredo.sosa@boushtech.net>
2024-09-16 16:46:55 -0500

    new xml to sql imp.

--- app/api/features/schemas/services_schemas.py
+++ app/api/features/schemas/services_schemas.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel, Field
 class JsonToSQLSchema(BaseModel):
     json_input: str = Field(..., description="The input JSON string containing the data structure to be converted into SQL.")
     sql_dbms: str = Field(..., description="The type of SQL DBMS for which the SQL script will be generated (e.g., MySQL, PostgreSQL, SQLite).")
+
+class XmlToSQLSchema(BaseModel):
+    xml_input: str = Field(..., description="The input XML string containing the data structure to be converted into SQL.")
+    sql_dbms: str = Field(..., description="The type of SQL DBMS for which the SQL script will be generated (e.g., MySQL, PostgreSQL, SQLite).")
